a46ebc6783bab38b397849678a3e2c3cc6393c30
sajid <sajid.siddiqueibt@gmail.com>
2019-12-19 02:43:31 +0500

    js added

---
+++ 3rd.js
@@ -0,0 +1,98 @@
+
+
+
+// //Question no 1....
+
+// var education = ["SSC","HSC","Bcom","MS","Mphill","Phd"];
+// var ln = education.length;
+// document.write("<h2>Qualification<br></h2>");
+// for(var i=1 ; i<ln; i++) {
+    
+//     document.write(i + ") " + education[i] + "<br>");
+
+// }
+
+// // Question no 2...  Percentage formula .. got/total*100
+
+//  var studentName = ["Michael","john","tony"];
+//  var score = [320 , 230 , 480];
+// for(var i = 0 ; i<=2; i++){
+//     document.write("Score of " +studentName[i] +". Percentage:" +score[i]/500*100 +"<br>");
+// }
+ 
+// //Question no 3  //only G
+// var colors = ["Red" , "Blue","Green","Yellow", "Orange","White"];
+// colors.splice(1,1, "purple");
+// document.write(colors);
+
+// //Question 4
+// var citylist = ["Lahore","Karachi","islamabad","Quetta","Peshawar"];
+// document.write("<h2>Cities list:</h2><br>");
+// document.write(citylist +"<br>");
+
+// document.write("<h2>Selected cities list :</h2><br>");
+// document.write(citylist[3] +" " +citylist[4]);
+
+
+// //Question no 5
+// var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// var set = [...new Set(arr1)];
+// document.write(set);
+
+// //Question no 6
+// var aCities = ["Karachi", "Lahore", "Islamabad", "Faisalabad"];
+// var o = ["th","st","nd","rd"];
+// document.write("1"+o[1] +" choice is " +aCities[0] +"<br>");
+// document.write("2"+o[2] +" choice is " +aCities[1] +"<br>");
+// document.write("3"+o[3] +" choice is " +aCities[2] +"<br>");
+
+// //Question no 7
+// var a = [10,20,4,40,60,70];
+// var b = [1,2,3,4,5,6,7,8,9,10];
+// var c = [...a , ...b];
+// var orderNum = [...new Set(c)];
+// document.write(orderNum);
+
+// //Question no 8
+// var counting  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// document.write(counting +"<br>");
+
+// var reverseCounting = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// document.write(reverseCounting +"<br>");
+
+// var even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+// document.write(even +"<br>");
+
+// var odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+// document.write(odd +"<br>");
+
+// var series = [ 2,"k", 4,"k", 6,"k", 8,"k", 10,"k", 12,"k", 14,"k", 16,"k", 18,"k", 20,"k"];
+// document.write(series +"<br>");
+
+// //Question 9
+
+// var large = [24, 53, 78, 91, 12];
+// document.write("<h3>Array item</h3>"+large +"<br>");
+
+// //var largest = Math.max(...[large]);
+// document.write("<h3>The largest number</h3>"+Math.max(...large) +"<br>");
+
+// //Question  no 9
+
+// var large = [24, 53, 78, 91, 12];
+// document.write("<h3>The largest number</h3>"+large +"<br>");
+// var max = large[0];
+// for(var i=0 ; i<large.length; i++){
+//     if(large[i]>=max){
+//         max=large[i]
+
+//     }
+// }
+// document.write("<h3>The largest number is </h3>" +max);
+
+//Question no 10
+// var a =  [20,53,78,4,91,12];
+// var sorted = a.sort();
+
+// document.write(a);
+
